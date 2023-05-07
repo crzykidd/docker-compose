4aade032af598413cd81b5ee2b39cfdbcc6ddc25
Matthew Andersen <mander@loginet.com>
2023-05-07 13:06:07 -0700

    removed debug

--- gitpull.sh
+++ gitpull.sh
@@ -5,7 +5,6 @@
 # directory as the folder to look through
 #REPOSITORIES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPOSITORIES=$HOME$"/git"
-echo $REPOSITORIES
 IFS=$'\n'
 
 for REPO in `ls "$REPOSITORIES/"`
